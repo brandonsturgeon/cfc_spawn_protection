ff06227fa617537b60ace9c07077dc4c88195b3c
Brandon Sturgeon <sturgeonb4@gmail.com>
2018-04-05 09:33:15 -0700

    [WIP] Rework

--- lua/autorun/server/sv_spawn_protection.lua
+++ lua/autorun/server/sv_spawn_protection.lua
@@ -2,27 +2,52 @@
 -- Time in seconds after moving for the first time that the player will lose spawn protection
 spawnProtectionMoveDelay = 1
 
+spawnProtectionMovementKeys = {}
+spawnProtectionMovementKeys[IN_JUMP] = true
+spawnProtectionMovementKeys[IN_MOVELEFT] = true
+spawnProtectionMovementKeys[IN_MOVERIGHT] = true
+spawnProtectionMovementKeys[IN_FORWARD] = true
+spawnProtectionMovementKeys[IN_BACK] = true
+	
+
 allowedSpawnWeapons = {
+	["Physics Gun"]       = true,
         ["weapon_physgun"]    = true,
         ["weapon_physcannon"] = true,
         ["gmod_tool"]         = true,
         ["gmod_camera"]       = true,
-        ["weapon_medkit"]     = true
+        ["weapon_medkit"]     = true,
         ["none"]              = true,
         ["laserpointer"]      = true,
         ["remotecontroller"]  = true
 }
 
 function setSpawnProtection( player )
-    player:SetNWBool("hasSpawnProtection", true)
-    player:GodEnable()
+    player:SetNWBool("hasSpawnProtection", false)
+end
+
+function setSpawnProtectionForPvPSpawn( player )
+	local playerIsInPvP = player:GetNWBool("PVPMode", false)
+	if ( playerIsInPvP ) then
+		setSpawnProtection( player )
+	end
 end
 hook.Remove("PlayerSpawn", "CFCsetSpawnProtection")
-hook.Add("PlayerSpawn", "CFCsetSpawnProtection", setSpawnProtection)
+hook.Add("PlayerSpawn", "CFCsetSpawnProtection", setSpawnProtectionForPvPSpawn)
 
 function removeSpawnProtection( player )
+	player:ChatPrint("You've left spawn protection")
     player:SetNWBool("hasSpawnProtection", false)
-    player:GodDisable()
+end
+
+function delayRemoveSpawnProtection( player, _delay )
+	local delay = _delay or spawnProtectionMoveDelay
+	player:SetNWBool("disablingSpawnProtection", true)
+	
+	timer.Simple(spawnProtectionMoveDelay, function ()
+		player:SetNWBool("disablingSpawnProtection", false)
+		removeSpawnProtection( player )
+	end)
 end
 
 function spawnProtectionWeaponChangeCheck( player, oldWeapon, newWeapon)
@@ -32,7 +57,7 @@ function spawnProtectionWeaponChangeCheck( player, oldWeapon, newWeapon)
           local playerLacksSpawnProtection = !player:GetNWBool("hasSpawnProtection", true)
 
           if (playerLacksSpawnProtection) then
-              local weaponIsDisallowed = !allowedSpawnWeapons[newWeapon]
+              local weaponIsDisallowed = !allowedSpawnWeapons[newWeapon:GetClass()]
 
               if ( weaponIsDisallowed ) then
                   removeSpawnProtection( player )
@@ -43,14 +68,45 @@ end
 hook.Remove("PlayerSwitchWeapon", "CFCspawnProtectionWeaponChange")
 hook.Add("PlayerSwitchWeapon", "CFCspawnProtectionWeaponChange", spawnProtectionWeaponChangeCheck)
 
-function spawnProtectionMoveCheck( player, _ )
-    local playerHasSpawnProtection = player:GetNWBool("hasSpawnProtection", false)
-  
-    if ( playerHasSpawnProtection ) then
-        timer.Simple(spawnProtectionMoveDelay, function ()
-            removeSpawnProtection( player )
-        end)
-    end
+function spawnProtectionMoveCheck( player, keyPressed )
+	local playerIsDisablingSpawnProtection = player:GetNWBool("disablingSpawnProtection", false)
+	
+	if ( !playerIsDisablingSpawnProtection ) then
+		local playerHasSpawnProtection = player:GetNWBool("hasSpawnProtection", false)
+	  
+		if ( playerHasSpawnProtection ) then
+			local playerIsMovingThemselves = spawnProtectionMovementKeys[keyPressed]
+
+			if ( playerIsMovingThemselves ) then
+				delayRemoveSpawnProtection( player )
+			end
+		end
+	end
 end
-hook.Remove("Move", "CFCspawnProtectionMoveCheck")
-hook.Add("Move", "CFCspawnProtectionMoveCheck", spawnProtectionMoveCheck)
+hook.Remove("KeyPress", "CFCspawnProtectionMoveCheck")
+hook.Add("KeyPress", "CFCspawnProtectionMoveCheck", spawnProtectionMoveCheck)
+
+function preventDamageDuringSpawnProtection( player, damageInfo )
+	local playerHasSpawnProtection = player:GetNWBool("hasSpawnProtection", false)
+	
+	if ( playerHasSpawnProtection ) then
+		player:ChatPrint("You have spawn protection")
+		damageInfo:SetDamage( 0 )
+		return false
+	end
+	
+end
+hook.Remove("EntityTakeDamage", "CFCpreventDamageDuringSpawnProtection")
+hook.Add("EntityTakeDamage", "CFCpreventDamageDuringSpawnProtection", preventDamageDuringSpawnProtection)
+
+-- Remove spawn protection when leaving PvP (just cleanup)
+hook.Remove("PlayerExitPvP", "CFCremoveSpawnProtectionOnExitPvP")
+hook.Add("PlayerExitPvP", "CFCremoveSpawnProtectionOnExitPvP", function(player)
+	removeSpawnProtection(player)
+end)
+
+-- Remove spawn protection when player enters vehicle
+hook.Remove("PlayerEnteredVehicle", "CFCremoveSpawnProtectionOnEnterVehicle")
+hook.Add("PlayerEnteredVehicle", "CFCremoveSpawnProtectionOnEnterVehicle", function(player)
+	removeSpawnProtection(player)
+end)
