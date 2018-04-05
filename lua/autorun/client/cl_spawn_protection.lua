a144cb8e4c3d6ebd46bdacbd9fa5176f90370b98
Brandon Sturgeon <sturgeonb4@gmail.com>
2018-04-05 00:26:05 -0700

    Add client logic

--- lua/autorun/client/cl_spawn_protection.lua
+++ lua/autorun/client/cl_spawn_protection.lua
@@ -1,1 +1,14 @@
---init
+spawnProtectionHaloColor = Color( 100, 100, 100)
+
+hook.Remove( "PreDrawHalos", "PVPHalos" )  
+hook.Add( "PreDrawHalos", "PVPHalos", function()
+    local lookent = LocalPlayer():GetEyeTrace().Entity
+    if ( IsValid(lookent) and lookent:IsPlayer() ) then
+        local target = lookent
+        local targetHasSpawnProtection = target:GetNWBool("hasSpawnProtection", false)
+
+        if ( targetHasSpawnProtection ) then
+            halo.Add( {target}, spawnProtectionHaloColor, 2, 1, 1 )
+        end
+    end
+end )
