2eaf4da96bc20c4eb0e1be7d8ba0730794c3b400
Brandon Sturgeon <sturgeonb4@gmail.com>
2018-04-06 01:51:27 -0700

    Rework

--- lua/autorun/server/sv_spawn_protection.lua
+++ lua/autorun/server/sv_spawn_protection.lua
@@ -2,6 +2,12 @@
 -- Time in seconds after moving for the first time that the player will lose spawn protection
 spawnProtectionMoveDelay = 1
 
+-- Time in seconds before spawn protection wears off if no action is taken
+spawnProtectionDecayTime = 10
+
+-- Prefix for the internal timer names - used to avoid timer collision
+spawnDecayPrefix = "cfc_spawn_decay_timer-"
+
 -- Table of key enums which are disallowed in spawn protection
 spawnProtectionMovementKeys = {}
 spawnProtectionMovementKeys[IN_JUMP]      = true
@@ -25,16 +31,46 @@ allowedSpawnWeapons = {
 
 -- Helpers / Wrappers
 
+local function setPlayerTransparent( player )
+	player:SetRenderMode( RENDERMODE_TRANSALPHA )
+	player:Fire( "alpha", 175, 0 )
+end
+
+local function setPlayerVisible( player )
+	player:SetRenderMode( RENDERMODE_NORMAL )
+	player:Fire( "alpha", 255, 0 )
+end
+
+local function playerDecayTimerIdentifier( player )
+	return spawnDecayPrefix .. player:SteamID64()
+end
+
 local function setSpawnProtection( player )
     player:SetNWBool("hasSpawnProtection", true)
+	setPlayerTransparent( player )
 end
 
 local function removeSpawnProtection( player )
-    player:ChatPrint("You've left spawn protection")
+    player:ChatPrint("You've lost spawn protection")
     player:SetNWBool("hasSpawnProtection", false)
+	setPlayerVisible( player )
+end
+
+local function createDecayTimer( player ) 
+	local playerIdentifer = playerDecayTimerIdentifier( player )
+	timer.Create( playerIdentifer, spawnProtectionDecayTime, 1, function()
+		removeSpawnProtection( player )
+	end)
+end
+
+local function removeDecayTimer( player )
+	player:ChatPrint("Removing decay timer")
+	local playerIdentifer = playerDecayTimerIdentifier( player )
+	timer.Remove( playerIdentifer )
 end
 
 local function delayRemoveSpawnProtection( player, _delay )
+	player:ChatPrint("delayRemoveSpawnProtection")
     local delay = _delay or spawnProtectionMoveDelay
     player:SetNWBool("disablingSpawnProtection", true)
 
@@ -49,7 +85,7 @@ local function playerIsInPvP( player )
 end
 
 local function playerHasSpawnProtection( player )
-    return player:GetNWBool("playerHasSpawnProtection", false)
+    return player:GetNWBool("hasSpawnProtection", false)
 end
 
 local function playerIsDisablingSpawnProtection( player )
@@ -57,26 +93,26 @@ local function playerIsDisablingSpawnProtection( player )
 end
 
 local function weaponIsAllowed( weapon )
-    return allowedSpawnWeapons[weapon:GetClass()] or false
+    return allowedSpawnWeapons[weapon:GetClass()]
 end
 
 local function keyVoidsSpawnProtection( keyCode )
-    return spawnProtectionMovementKeys[keyCode] or false
+    return spawnProtectionMovementKeys[keyCode]
 end
-
 
 -- Hook functions --
 
 local function setSpawnProtectionForPvPSpawn( player )
     if ( playerIsInPvP( player ) ) then
         setSpawnProtection( player )
+		createDecayTimer( player )
     end
 end
 
 local function spawnProtectionWeaponChangeCheck( player, oldWeapon, newWeapon)
-    if (playerIsInPvP( player )) then
+    if ( playerIsInPvP( player ) ) then
 
-        if ( !playerHasSpawnProtection( player ) ) then
+        if ( playerHasSpawnProtection( player ) ) then
 
             if ( !weaponIsAllowed( newWeapon ) ) then
                 removeSpawnProtection( player )
@@ -87,9 +123,8 @@ end
 
 local function spawnProtectionMoveCheck( player, keyCode )
     if ( !playerIsDisablingSpawnProtection( player ) ) then
-
+		
         if ( playerHasSpawnProtection( player ) ) then
-
             local playerIsMovingThemselves = keyVoidsSpawnProtection( keyCode )
 
             if ( playerIsMovingThemselves ) then
@@ -100,9 +135,8 @@ local function spawnProtectionMoveCheck( player, keyCode )
 end
 
 local function preventDamageDuringSpawnProtection( player, damageInfo )
-    local playerHasSpawnProtection = player:GetNWBool("hasSpawnProtection", false)
 
-    if ( playerHasSpawnProtection ) then
+    if ( playerHasSpawnProtection( player ) ) then
         player:ChatPrint("You have spawn protection")
         damageInfo:SetDamage( 0 )
         return false
