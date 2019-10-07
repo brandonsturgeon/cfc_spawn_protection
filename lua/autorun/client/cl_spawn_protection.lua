42d8f9169d2ef442dac591465b5c4eca33e89127
Brandon Sturgeon <sturgeonb4@gmail.com>
2019-10-07 18:55:48 -0400

    Various whitespace fixes

--- lua/autorun/client/cl_spawn_protection.lua
+++ lua/autorun/client/cl_spawn_protection.lua
@@ -1,11 +1,11 @@
-spawnProtectionHaloColor = Color( 100, 100, 100)
+spawnProtectionHaloColor = Color( 100, 100, 100 )
 
-hook.Remove( "PreDrawHalos", "spawnProtectionHalos" )  
+hook.Remove( "PreDrawHalos", "spawnProtectionHalos" )
 hook.Add( "PreDrawHalos", "spawnProtectionHalos", function()
     local lookent = LocalPlayer():GetEyeTrace().Entity
-    if ( IsValid(lookent) and lookent:IsPlayer() ) then
+    if ( IsValid( lookent ) and lookent:IsPlayer() ) then
         local target = lookent
-        local targetHasSpawnProtection = target:GetNWBool("hasSpawnProtection", false)
+        local targetHasSpawnProtection = target:GetNWBool( "hasSpawnProtection", false )
 
         if ( targetHasSpawnProtection ) then
             halo.Add( {target}, spawnProtectionHaloColor, 2, 1, 1 )
